b5f0790af0144fc196b6bf7ac04bccf291b9dfec
jackpopp <jack.popp@gmail.com>
2013-12-11 14:58:23 +0000

    Added a proxy class to make the web requests for us, this should fix problems with cross domain requests.

---
+++ request.php
@@ -0,0 +1,8 @@
+<?php
+
+require 'request/src/proxy.php';
+
+use Request\Src\Proxy as Proxy
+
+$proxy = new Proxy();
+echo $proxy->requestPage();
---
+++ request/src/proxy.php
@@ -0,0 +1,93 @@
+<?php namespace Request\Src;
+
+use Exception;
+
+class Proxy 
+{
+	private $URI;
+	private $result;
+
+	function __construct()
+	{
+		try{
+			if (empty($_POST['uri']))
+				throw new Exception("Please post a uri");
+
+			$this->setURI($_POST['uri']);
+
+			if ( ! filter_var($this->getURI(), FILTER_VALIDATE_URL, FILTER_FLAG_HOST_REQUIRED))
+				throw new Exception("Please provide valid uri");	
+			
+		}
+		catch (Exception $e){
+			echo $e->getMessage();
+
+		}
+	}
+
+	/**
+	* Sets the URI property
+	*
+	*	@return $this
+	*/
+
+	public function setURI($URI)
+	{
+		$this->URI = $URI;
+		return $this;
+	}
+
+	/**
+	*	Gets the URI property
+	*
+	*	@return string
+	*/
+
+	public function getURI()
+	{
+		return $this->URI;
+	}
+
+	/**
+	* Sets the result property
+	*
+	*	@return $this
+	*/
+
+	public function setResult($result)
+	{
+		$this->result = $result;
+		return $this;
+	}
+
+	/**
+	*	Gets the result property
+	*
+	*	@return html page
+	*/
+
+	public function getResult()
+	{
+		return $this->result;
+	}
+
+	/**
+	*	Requests a webpage via curl using the uri thats been posted to the object
+	*
+	*	@return html page 
+	*/
+	public function requestPage()
+	{
+		$ch = curl_init(); 
+        // set url 
+        curl_setopt($ch, CURLOPT_URL, $this->getURI()); 
+        //return the transfer as a string 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+        // $output contains the output string 
+        $result = curl_exec($ch);
+        // close curl resource to free up system resources 
+        curl_close($ch); 
+
+        return $result;
+	}
+}
